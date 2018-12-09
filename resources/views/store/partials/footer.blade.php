e2cf3f3d43664cc5502f1a45e169aa318962c754
GabrielQuesadaAraya <gabrielquesadajgqa@gmail.com>
2018-12-09 15:43:30 -0600

    Ajustes visuales en la seccion cliente (Carrito de  compras)

--- resources/views/store/partials/footer.blade.php
+++ resources/views/store/partials/footer.blade.php
@@ -1,18 +1,19 @@
 <footer class="container-fluid">
 	<div class="row">
 		<div class="col-md-4">
-			<h3>My Laravel Store</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores ratione minus, voluptates dolore soluta harum. Nobis neque doloribus corrupti iusto rem beatae libero, incidunt animi nemo voluptatem dolores accusantium magni ad ipsum eum repellendus vero deleniti velit molestiae quaerat, veniam ducimus labore sapiente? Commodi ad, pariatur. Ab consectetur, quasi!</p>
+			<h3 style="background:white;">Carrito de Compras</h3>
+			<p style="background:;">Basicamente una tienda en linea, en donde tu como cliente puedes añadir varios productos a tu carrito, una ves desidido de que productos quieres comprar, simplemente vas con tu cuenta paypal y pagas los productos pedidos.</p>
 		</div>
 		<div class="col-md-4">
-			<h3>Desarrollado por:</h3>
+			<h3 style="background:white;">Desarrollado por:</h3>
 			<div class="author-info">
-				<img src="https://pbs.twimg.com/profile_images/592393458469380096/T4twnQ3Q.png" alt="" class="avatar">
-				<p><a href="#">@ovedfs</a> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum excepturi possimus tempora porro, a est atque? Rem eaque, ullam, dolore fugit cupiditate odio ipsum fugiat quae, error accusantium corporis harum voluptatem perspiciatis dolorem pariatur iste id temporibus voluptatum enim dicta.</p>
+				<img src="https://scontent.fsjo8-1.fna.fbcdn.net/v/t1.0-9/25443037_1873132992999090_3450979640571324938_n.jpg?_nc_cat=109&_nc_ht=scontent.fsjo8-1.fna&oh=37bdfddd663a0f39e6fb7cccc4cdf10f&oe=5C9274B7" alt="" class="avatar">
+				<img src="https://scontent.fsjo8-1.fna.fbcdn.net/v/t1.0-9/44732009_489296658251776_8109718911429115904_n.jpg?_nc_cat=103&_nc_ht=scontent.fsjo8-1.fna&oh=b0a8975799c7d8634774898b5751d169&oe=5C98A9E9" alt="" class="avatar">
+				<p><a href="#">Gabriel Quesada Y Antony Calderon</a></p>
 			</div>
 		</div>
 		<div class="col-md-4">
-			<h3>Siguenos</h3>
+			<h3 style="background:white;">Siguenos en nuetras redes sociales</h3>
 			<ul class="redes">
 				<li>
 					<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
@@ -30,8 +31,9 @@
 					<a href="#"><i class="fa fa-youtube-square fa-2x"></i></a>
 				</li>
 			</ul>
-			<h3>Escribenos:</h3>
-			<i class="fa fa-at"></i> <a href="#">ovedfs@gmail.com</a>
+			<h3 style="background:white;">Escribenos:</h3>
+			<i class="fa fa-at"></i> <a href="#">gabrielQQQuesada@gmail.com</a>
+			<i class="fa fa-at"></i> <a href="#">ElAntonyHuevosDePelusa@gmail.com</a>
 		</div>
 	</div>
 </footer>
--- resources/views/store/partials/nav.blade.php
+++ resources/views/store/partials/nav.blade.php
@@ -7,11 +7,11 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand main-title" href="{{ route('home') }}">FiSO</a>
+      <a class="navbar-brand main-title" href="{{ route('home') }}">PuertoCel</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <p class="navbar-text">My Laravel Store</p>
+      <p class="navbar-text">Carrito de compras</p>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="{{ route('cart-show') }}"><i class="fa fa-shopping-cart"></i></a></li>
         <li><a href="#">Conocenos</a></li>
